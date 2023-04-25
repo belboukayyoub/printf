b16b3da8c11b21a6be0fb1d5cd3ea66a646d5e33
Belbouk Ayyoub <belboukayyoub@gmail.com>
2023-04-25 16:50:37 -0700

    Code refactoring

---
+++ helper.c
@@ -0,0 +1,71 @@
+#include "main.h"
+
+/**
+ * print_integer - prints an integer
+ * @num: intut int
+ *
+ * Return: nbr of chart printed
+ */
+
+unsigned int print_integer(int num)
+{
+	unsigned int i = 0, printed_char = 0, n;
+	char *digits = (char *)malloc(sizeof(char));
+
+	if (digits == NULL)
+		return (-1);
+
+	if (num < 0)
+	{
+		_putchar('-');
+		printed_char++;
+		n = num * (-1);
+	}
+	else
+		n = num;
+	if (n == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+
+	while (n > 0)
+	{
+		digits[i++] = n % 10 + '0';
+		digits = (char *)_realloc(digits, i, i + 1);
+		n = n / 10;
+	}
+	while (i > 0)
+	{
+		_putchar(digits[--i]);
+		printed_char++;
+	}
+	free(digits);
+	return (printed_char);
+}
+
+/**
+ * print_string - print a static string
+ *
+ * @s: input string
+ *
+ * Return: nbr of char printed
+ */
+unsigned int print_string(char *s)
+{
+	unsigned int i, printed_char = 0;
+
+	if (s == NULL)
+	{
+		return (print_string("(null)"));
+	}
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		_putchar(s[i]);
+		printed_char++;
+	}
+
+	return (printed_char);
+}
+
--- main.h
+++ main.h
@@ -1,7 +1,15 @@
 #ifndef PRINTF_H
 #define PRINTF_H
 
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 int _putchar(char c);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+unsigned int print_integer(int num);
+unsigned int print_string(char *s);
 int _printf(const char *format, ...);
 
 #endif
