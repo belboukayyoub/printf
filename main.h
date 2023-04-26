1b975ed2238a1e8beb7118f7b4e0f20619e2ba18
Belbouk Ayyoub <belboukayyoub@gmail.com>
2023-04-26 15:01:33 -0700

    Rfactoring code

--- main.h
+++ main.h
@@ -10,7 +10,7 @@ int _putchar(char c);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 unsigned int print_integer(int num);
 unsigned int print_string(char *s);
-unsigned int print_binary(unsigned int n);
+unsigned int print_base(unsigned int n, int base);
 int _printf(const char *format, ...);
 
 #endif
